f4a4c7fe76534efefd21ce306e3cff24030048d8
walissonsou <walissonsouza96@hotmail.com>
2022-07-18 12:52:07 -0300

    create api's user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
-import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent,
-    MeuPrimeiro2Component
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/usuarios/usuario.model.ts
@@ -0,0 +1,18 @@
+declare module namespace {
+
+  export interface Usuario {
+      id: number;
+      email: string;
+      first_name: string;
+      last_name: string;
+      avatar: string;
+  }
+
+  export interface ResponseUsuario {
+      page: number;
+      per_page: number;
+      total: number;
+      total_pages: number;
+      data: Usuario[];
+  }
+}
